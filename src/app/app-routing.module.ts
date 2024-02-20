648c88f526c742b5bfea02fb79a883bfbc5acf8f
Nouné BILBOULIAN <nouneblb@gmail.com>
2024-02-20 17:03:19 +0100

    ignore bug cloud

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AppComponent } from './app.component';
+
 import { HomeComponent } from './pages/home/home.component';
 import { ServicesPageComponent } from './pages/services-page/services-page.component';
 import { OccasionsComponent } from './pages/occasions/occasions.component';

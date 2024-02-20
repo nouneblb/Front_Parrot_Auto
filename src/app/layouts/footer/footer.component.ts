c70204131ef2f67e6be711ac1b5435aae08bb76b
Nouné BILBOULIAN <nouneblb@gmail.com>
2024-02-20 15:13:38 +0100

    styles + ts for footer

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  schedules = [
+    { day: 'Lun - Ven', timeRanges: ['08:00 - 13:00', '14:00 - 20:00'] },
+    { day: 'Sam', timeRanges: ['9:00 - 18:00'] },
+    { day: 'Dim', closed: true },
+  ];
 
+  constructor(private horaireService: HoraireService) {}
+
+  saveHoraire():void {
+    this.horaireService.saveHoraires(this.schedules)
+      .subscribe(response => {
+        console.log('Horaires enregistrés avec succès !');
+      }, error => {
+        console.error('Erreur lors de l\'enregistrement des horaires:',error);
+      });
+  }
 }
+

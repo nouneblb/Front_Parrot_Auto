004f5c5d68783fc8f38129babcf8692412e6ebc9
Nouné BILBOULIAN <nouneblb@gmail.com>
2024-02-20 15:47:35 +0100

    delete other imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',

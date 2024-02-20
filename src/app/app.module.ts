324c288a386ca179e9c74eafcfd52b812f1809f5
Nouné BILBOULIAN <nouneblb@gmail.com>
2024-02-20 02:03:39 +0100

    import httpclient module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 
 

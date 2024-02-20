bd82ffb4c1134a287c70f500949534bfa46caec4
Nouné BILBOULIAN <nouneblb@gmail.com>
2024-02-20 15:06:48 +0100

    navbar update style and TS

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'app-header',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
 
+export class HeaderComponent {
+  isMenuOpen = false;
+  menuClass = '';
+
+  constructor(private router: Router) {}
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    this.menuClass = this.isMenuOpen ? 'show' : '';
+  }
+
+  closeMenuAndNavigate(route: string) {
+    this.isMenuOpen = false;
+    this.router.navigate([route]);
+  }
 }
